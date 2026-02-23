97c2506e1218a42aed2c9d65445e566767c87633
n-ille <agnes.petla@escola.pr.gov.br>
2026-02-23 11:14:03 -0300

    Add files via upload

---
+++ dsb.py
@@ -0,0 +1,26 @@
+### Atividade 1 - Nivelamento ###
+# Objetivo: Criar sua autodescrição utilizando variáveis e função print().
+# 1 - Pesquise o que é autodescrição e para quê ela serve.
+# 2 - Crie as seguintes variáveis:
+nome = "agnes"
+idade = 16
+altura = 1.63
+genero = "feminino"
+cor_de_pele = "branca"
+cor_de_cabelo = "preto"
+tamanho_cabelo = "medio"
+estilo_cabelo = "cacheado com franja"
+# 3 - Crie 2 variáveis: uma armazena o curso que você faz, a outra armazena o seu hobby favorito.
+curso = "desenvolvimento de sistemas"
+hobby = "musica"
+# 4 - Utilizando a função print(), mostre o texto completo da sua autodescrição, utilizando as variáveis que você criou!
+# Exemplo: "Olá, meu nome é Fulano, tenho 999 anos e 1.90 de altura... (continue até usar todas as variáveis)"
+print( "olá, meu nome é ", nome, "tenho ", idade, " anos e minha altura é ", altura, ". sou do genero ", genero, " sou ", cor_de_pele, " do cabelo ", cor_de_cabelo, estilo_cabelo, " meu cabelo é ", tamanho_cabelo, "." " sou do curso ", curso, "e meu hobby é", hobby, ".")
+## Forma de avaliação: ##
+# 0,5 ponto = Preenchimento correto das variáveis do item 2
+# 0,5 ponto = Criação das variáveis do item 3
+# 1,0 ponto = Coerência do texto e uso correto das variáveis na função print()
+# TOTAL: 2,0 pontos
+
+## Entrega: ##
+# O aluno deve criar um novo repositório no GitHub, com o nome "1TRI-DesenvolvimentoSistemas" e realizar upload do arquivo .py.
